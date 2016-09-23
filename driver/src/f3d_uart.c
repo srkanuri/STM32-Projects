2047bce3bf57b2650b184a31bd8b5bff553cdcaa
srkanuri <srkanuri@iu.edu>
2016-09-23 09:15:44 -0400

    updated f3d_uart.c

--- driver/src/f3d_uart.c
+++ driver/src/f3d_uart.c
@@ -17,34 +17,32 @@
 //Parameters: None
 //Return Type: void
 void f3d_uart_init(void) {
-  GPIO_InitTypeDef GPIO_InitStructure;
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-  GPIO_StructInit(&GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOC,&GPIO_InitStructure);
-  
-  GPIO_StructInit(&GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOC,&GPIO_InitStructure);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+  	GPIO_StructInit(&GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOC,&GPIO_InitStructure);
+	GPIO_PinAFConfig(GPIOC,4,GPIO_AF_7);
 
-  GPIO_PinAFConfig(GPIOC,4,GPIO_AF_7);
-  GPIO_PinAFConfig(GPIOC,5,GPIO_AF_7);
-  
-  USART_InitTypeDef USART_InitStructure;
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-  USART_StructInit(&USART_InitStructure);
-  USART_InitStructure.USART_BaudRate = 9600;
-  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-  USART_Init(USART1 ,&USART_InitStructure);
-  USART_Cmd(USART1 , ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOC,&GPIO_InitStructure);
+	GPIO_PinAFConfig(GPIOC,5,GPIO_AF_7);
+
+	USART_InitTypeDef USART_InitStructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	USART_StructInit(&USART_InitStructure);
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART1 ,&USART_InitStructure);
+	USART_Cmd(USART1 , ENABLE);
 }
 
 //Function name: putchar
@@ -52,18 +50,18 @@ void f3d_uart_init(void) {
 //Parameters: int
 //Return Type: int
 int putchar(int c) {
-  while (USART_GetFlagStatus(USART1,USART_FLAG_TXE) == (uint16_t)RESET);
-  USART_SendData(USART1, c);
-}
+	while (USART_GetFlagStatus(USART1,USART_FLAG_TXE) == (uint16_t)RESET);
+		USART_SendData(USART1, c);
+} 
 
 //Function name: getchar
 //Description: gets a character
 //Parameters: None
 //Return Type: int
 int getchar(void) {
-  while (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == (uint16_t)RESET);
-  int c = USART_ReceiveData(USART1);
-  return c;
+	int c;
+	while (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == (uint16_t)RESET);
+		c = USART_ReceiveData(USART1);
 }
 
 //Function name: putstring
@@ -71,10 +69,11 @@ int getchar(void) {
 //Parameters: String pointer
 //Return Type: void
 void putstring(char *s) {
-  while(*s != '\0'){
-    putchar(*s);
-    s++;
-  }
+	int i = 0;
+	while(s[i] != '\0') {
+		putchar(s[i]);
+		i++;	
+	}
 }
 
 /* f3d_uart.c ends here */
